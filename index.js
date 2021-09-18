ad82153ac2830ce22187d71ea211dfcbc8b1191b
Geoffrey Wiseman <geoffrey.wiseman@codiform.com>
2021-09-18 19:57:17 -0400

    Old AMIs
    Getting ready to release the first draft of Old AMIs. It's not a super-sophisticated tool, but it's in a place where I can start sharing and if people find it useful, enhance it from there.
    
    Signed-off-by: Geoffrey Wiseman <geoffrey.wiseman@codiform.com>

---
+++ index.js
@@ -0,0 +1,12 @@
+const {EC2Client, DescribeImagesCommand} = require("@aws-sdk/client-ec2");
+const {getAmiDates, getAmis} = require("./lib/aws");
+const {printBanner, printAmi} = require("./lib/display");
+
+(async function () {
+    printBanner();
+    const amis = await getAmis();
+    const dates = await getAmiDates(Object.keys(amis));
+    for( ami of Object.keys(amis) ) {
+        printAmi(ami, dates[ami], amis[ami].join(', '));
+    }
+})();
